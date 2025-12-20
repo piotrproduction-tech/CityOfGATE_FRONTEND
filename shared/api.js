704ee9cf37016a18a18c60bab54a775822cb483b
piotrjacko <piotr.dgproduction@gmail.com>
2025-12-20 18:36:33 +0100

    Update api.js

--- shared/api.js
+++ shared/api.js
@@ -1,14 +1,12 @@
 /**
  * Dynamiczne pobieranie URL backendu
  */
-let API_BASE = null;
-
 async function loadBackendUrl() {
   console.log("[API] Fetching backend URL...");
 
   try {
     const res = await fetch(
-      "https://script.google.com/macros/s/AKfycbwYD60yf7_h0Ucu99-sfYGLnivbpM5jPZYKHxOMCyWAGSthFh3bDE4YqZbaajtO-KuP9Q/exec?path=system/webapp-url"
+      "https://script.google.com/macros/s/AKfycbyRt3DGCaDMNQ4pGyGf3EoLHpfUE8v3_or49gELyHhqtxcdjFSMxFzkrBeVw5WZ1IWRxA/exec?path=system/webapp-url"
     );
 
     const data = await res.json();
@@ -22,3 +20,4 @@ async function loadBackendUrl() {
 
 // automatyczne pobranie backendu przy starcie
 loadBackendUrl();
+
