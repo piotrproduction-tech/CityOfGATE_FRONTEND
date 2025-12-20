2391a95b350dd1ef898877c17901395136e11e2d
piotrjacko <piotr.dgproduction@gmail.com>
2025-12-20 20:37:40 +0100

    Update api.js

--- shared/api.js
+++ shared/api.js
@@ -14,7 +14,8 @@ async function loadBackendUrl() {
 
   try {
     const res = await fetch(
-  "https://script.google.com/macros/s/AKfycbz-3Fkr1ZWq2UO5ekY2J8AjsDptx8bsRRNoS-S3U7NMn8q3RD6jc60rFKaJFqliipjbRw/exec"
+  "const res = await fetch(
+  "https://script.google.com/macros/s/AKfycbz-3Fkr1ZWq2UO5ekY2J8AjsDptx8bsRRNoS-S3U7NMn8q3RD6jc60rFKaJFqliipjbRw/exec?path=system/webapp-url"
 );
 
     const data = await res.json();
@@ -87,6 +88,7 @@ async function api_getTransactions() {
 async function api_getBalance() {
   return apiGet("budget/balance/get");
 }
+
 
 
 
